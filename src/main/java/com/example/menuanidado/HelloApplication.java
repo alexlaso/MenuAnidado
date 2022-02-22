ea06dfedf0e4d557248feedd65744b943849d054
AlejandroLasoSalido <alex_laso_laso@hotmai.com>
2022-02-22 14:06:00 +0100

    Version final practica 1

--- src/main/java/com/example/menuanidado/HelloApplication.java
+++ src/main/java/com/example/menuanidado/HelloApplication.java
@@ -2,15 +2,23 @@ package com.example.menuanidado;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VistaPrincipal.fxml"));
+        Locale spanish = new Locale("es", "ES");
+        Locale english = new Locale("en", "UK");
+        ResourceBundle rBSpanish = ResourceBundle.getBundle("com/example/menuanidado/i18n.strings",spanish);
+        ResourceBundle rBEnglish = ResourceBundle.getBundle("com/example/menuanidado/i18n.strings",english);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("VistaPrincipal.fxml"), rBSpanish);
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("UwU");
         stage.setScene(scene);
